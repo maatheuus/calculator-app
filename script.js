f3b95412df61efa82c76426978c8fad8e26c0192
MATHEUS <matts14smkd@gmail.com>
2023-08-31 11:43:26 -0300

    atualizando classes e ids. add o script e finalizando o style

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+const inputData = document.querySelectorAll('input');
+const entrada = document.querySelectorAll('.pontilhado');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function (e) {
+  console.log(e);
+});
