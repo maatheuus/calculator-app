e5982367b27ef3114a39f50a0764b11e9fca4388
MATHEUS <matts14smkd@gmail.com>
2023-09-01 11:59:45 -0300

    mexendo no script

--- script.js
+++ script.js
@@ -2,7 +2,65 @@
 const inputData = document.querySelectorAll('input');
 const entrada = document.querySelectorAll('.pontilhado');
 const btn = document.querySelector('.btn');
+const saida = document.getElementById('saida--display');
 
-btn.addEventListener('click', function (e) {
-  console.log(e);
-});
+const renderHYMLOne = function () {
+  const html1 = `
+<div class="saida">
+    <div class="container-saida">
+        <span class="pontilhado"></span>
+        <span class="pontilhado"></span>
+    </div>
+    years
+</div>
+
+<div class="saida">
+    <div class="container-saida">
+        <span class="pontilhado"></span>
+        <span class="pontilhado"></span>
+    </div>
+    months
+</div>
+
+<div class="saida">
+    <div class="container-saida">
+        <span class="pontilhado"></span>
+        <span class="pontilhado"></span>
+    </div>
+    days
+</div>
+    `;
+
+  saida.insertAdjacentHTML('beforeend', html1);
+};
+renderHYMLOne();
+
+const renderHYMLTwo = function (years, months, days) {
+  const html2 = `
+    <div class="saida">
+    <div class="container-saida">${years}</div>
+    years
+  </div>
+  
+  <div class="saida">
+    <div class="container-saida">${months}</div>
+    months
+  </div>
+  
+  <div class="saida">
+    <div class="container-saida">${days}</div>
+    days
+  </div>
+      `;
+};
+// inputData.forEach(data =>
+//   data.addEventListener('keydown', function (e) {
+//     // console.log(e);
+//     if (e.key === 'Enter') {
+//       console.log(data.value);
+//       entrada.forEach(ent => {
+//         entrada.replace(ent, data.value);
+//       });
+//     }
+//   })
+// );
