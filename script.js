74bdec84cfd5d07f74738452aa928fda4cd82a3a
MATHEUS <matts14smkd@gmail.com>
2023-10-18 12:03:08 -0300

    refatorando o código

--- script.js
+++ script.js
@@ -2,18 +2,14 @@
 //selecionando os elementos
 const container = document.getElementById('container--display');
 const containerInput = document.getElementById('container--input');
-const inputData = document.querySelectorAll('input');
-const btn = document.querySelector('.btn');
 const year = document.getElementById('input--year');
 const month = document.getElementById('input--month');
 const days = document.getElementById('input--day');
-const error = document.getElementById('conatiner--error');
-const dataStyle = document.querySelectorAll('.data');
 
 ////////////////////////////////////////////////////////////
 // Substituindo o HTML de entrada pelo da saída
 const html = {
-  HTMLdisplay() {
+  renderDisplay() {
     const html = `
     <div id="display">
       <div class="saida">
@@ -35,7 +31,7 @@ const html = {
 
     container.insertAdjacentHTML('beforeend', html);
   },
-  HTMLinput(days, months, years) {
+  renderInput(days, months, years) {
     const html = `
       <div id="display--result">
         <div class="saida">
@@ -56,59 +52,45 @@ const html = {
     container.insertAdjacentHTML('beforeend', html);
   },
 };
-html.HTMLdisplay();
+html.renderDisplay();
 ////////////////////////////////////////////////////////////
 // Erros de entrada
-const htmlData = function (msgError = 'This field is required') {
-  const a = `
-  <div class="day-error">${msgError}</div>
-  <div class="month-error">${msgError}</div> 
-  <div class="year-error">${msgError}</div> 
-`;
-  error.insertAdjacentHTML('beforeend', a);
-};
+const error = document.getElementById('conatiner--error');
 
 const err = {
   dayError(msgError) {
+    days.style.border = '1px solid red';
+    document.querySelector('.data--day').style.color = 'red';
+
     error.insertAdjacentHTML(
       'beforeend',
       `<div class="day-error">${msgError}</div> `
     );
-    days.removeAttribute('style');
-    days.style.border = '1px solid red';
-
-    document.querySelector('.data--day').style.color = 'red';
   },
   monthError(msgError) {
+    month.style.border = '1px solid red';
+    document.querySelector('.data--month').style.color = 'red';
+
     error.insertAdjacentHTML(
       'beforeend',
       `<div class="month-error">${msgError}</div> `
     );
-    month.removeAttribute('style');
-    month.style.border = '1px solid red';
-    document.querySelector('.data--month').style.color = 'red';
   },
-  yearError(msgError) {
-    error.insertAdjacentHTML(
-      'beforeend',
-      `<div class="year-error">${msgError}</div> `
-    );
-    year.removeAttribute('style');
-    year.style.border = '1px solid red';
-    document.querySelector('.data--year').style.color = 'red';
-  },
-  _yearError(msgError) {
-    error.insertAdjacentHTML(
-      'beforeend',
-      `<div class="year-error">${msgError}</div> `
-    );
-    year.removeAttribute('style');
+  yearError(type, msgError) {
+    type === 'year'
+      ? error.insertAdjacentHTML(
+          'beforeend',
+          `<div class="year-error">${msgError}</div> `
+        )
+      : '';
+
     year.style.border = '1px solid red';
     document.querySelector('.data--year').style.color = 'red';
   },
 };
 ////////////////////////////////////////////////////////////
 // Limpando as entradas
+const inputData = document.querySelectorAll('input');
 const timeOut = {
   cleanDays() {
     // prettier-ignore
@@ -138,7 +120,7 @@ const timeOut = {
         sty.style.border = '1px solid #808080ba';
       });
       // prettier-ignore
-      dataStyle.forEach(color => {
+      document.querySelectorAll('.data').forEach(color => {
         color.style.color = '#9a9999';
       });
     }, 2000);
@@ -148,7 +130,6 @@ const timeOut = {
 // Lógica
 const date = new Date();
 const lastDay = new Date(date.getFullYear(), month.value + 1, 0);
-const lastDai = new Date(date.getFullYear(), month.value + 1, 0);
 
 function validInputs() {
   document.getElementById('container--display').innerHTML = '';
@@ -161,16 +142,16 @@ function validInputs() {
   // testando se o dia e o mês é menor que 0
   if (resDay < 0) {
     resMonth = date.getMonth() + 1 - 1 - month.value;
-    html.HTMLinput(sum, resMonth, resYear);
+    html.renderInput(sum, resMonth, resYear);
   } else if (resMonth < 0) {
     resMonth += 12;
     resYear -= 1;
-    html.HTMLinput(resDay, resMonth, resYear);
+    html.renderInput(resDay, resMonth, resYear);
   } else {
-    html.HTMLinput(resDay, resMonth, resYear);
+    html.renderInput(resDay, resMonth, resYear);
   }
 
-  // Testando se o tamnho é maior para ajustar a letra
+  // Testando se o tamanho é maior para ajustar a letra
   if (resDay.valueOf() >= 10 || resDay.valueOf() <= 0)
     document.querySelector('.container--days').style.width = '64px';
   else document.querySelector('.container--days').style.width = '32px';
@@ -184,79 +165,68 @@ function validInputs() {
   else if (resYear.valueOf() >= 10)
     document.querySelector('.container--year').style.width = '60px';
   else document.querySelector('.container--year').style.width = '32px';
-
-  // else document.querySelector('.container--year').style.width = '62px';
 }
+// Erros de entrada
 
-btn.addEventListener('click', function () {
-  // Possíveis erros de entrada
+function inputErrors() {
   timeOut.changeColor();
-
-  if (+days.value == ' ' && +month.value == ' ' && +year.value == ' ') {
-    htmlData();
-  } else if (
-    days.value.length != 2 ||
-    days.value < 0 ||
-    days.value > lastDay.getDate() ||
-    days.value == 0 ||
-    !Number.isFinite(+days.value)
-  ) {
+  // Possíveis erros de entrada
+  if (days.value < 0 || days.value == '' || days.value > lastDay.getDate()) {
     err.dayError('Must be a valid day');
-    timeOut.cleanDays(2);
+    timeOut.cleanDays();
   } else if (
-    month.value.length != 2 ||
+    month.value.length > 2 ||
     month.value < 0 ||
-    month.value > 12 ||
     month.value == 0 ||
-    !Number.isFinite(+month.value)
+    month.value > 12
   ) {
     err.monthError('Must be a valid month');
-    timeOut.cleanMonth(2);
-  } else if (
-    year.value.length != 4 ||
-    year.value < 0 ||
-    year.value == 0 ||
-    !Number.isFinite(+year.value)
-  ) {
-    err.yearError('Must be a valid year');
-    timeOut.cleanYear(2);
-  } else if (
-    Number.isFinite(+year.value) &&
-    Number.isFinite(+month.value) &&
-    Number.isFinite(+days.value)
-  ) {
-    //testando se o ano está no passado
-    if (year.value > date.getFullYear() || !Number.isFinite(+year.value)) {
-      err._yearError('Must be in the past');
-      timeOut.cleanYear(2);
-    } else {
-      // Exibindo as entradas
-      validInputs();
-
-      // Animação dos números
-      let containerSaida = document.querySelectorAll('.container-saida');
-      let interval = 1000;
-
-      containerSaida.forEach(containerValue => {
-        let startValue = 0;
-        let endValue = parseInt(containerValue.getAttribute('data-val'));
-        let duration = Math.floor(interval / endValue);
-
-        let counter = setInterval(function () {
-          startValue += 1;
-          containerValue.textContent = startValue;
-          if (startValue === endValue) {
-            clearInterval(counter);
-          } else if (endValue == 0 || endValue < 0) {
-            clearInterval(counter);
-          }
-          if (endValue == 0) {
-            startValue += 0;
-            containerValue.textContent = endValue;
-            clearInterval(counter);
-          }
-        }, duration);
-      });
-    }
+    timeOut.cleanMonth();
+  } else if (year.value.length !== 4 || year.value < 0) {
+    err.yearError('year', 'Must be a valid year');
+    timeOut.cleanYear();
   }
-});
+  //testando se o ano está no passado
+  else if (year.value > date.getFullYear()) {
+    err.yearError('year', 'Must be in the past');
+    timeOut.cleanYear();
+  } else {
+    validInputs();
+
+    // Animação dos números
+    let containerSaida = document.querySelectorAll('.container-saida');
+    let interval = 1000;
+
+    containerSaida.forEach(containerValue => {
+      let startValue = 0;
+      let endValue = parseInt(containerValue.getAttribute('data-val'));
+      let duration = Math.floor(interval / endValue);
+
+      let counter = setInterval(function () {
+        startValue += 1;
+        containerValue.textContent = startValue;
+        if (startValue === endValue) {
+          clearInterval(counter);
+        } else if (endValue == 0 || endValue < 0) {
+          clearInterval(counter);
+        }
+        if (endValue == 0) {
+          startValue += 0;
+          containerValue.textContent = endValue;
+          clearInterval(counter);
+        }
+      }, duration);
+    });
+  }
+}
+
+const getForm = document.getElementById('container--input');
+
+const init = function () {
+  getForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    this.blur();
+    inputErrors();
+  });
+};
+init();
